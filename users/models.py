8af04a9bf68a11d93df7e4280eaa028bf676b195
Juan David Díaz <juandiazr95@gmail.com>
2022-04-04 18:55:32 -0500

    creando modelos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+
+    gender = models.CharField('Gender', max_length=50)
+    full_name = models.CharField('Full Name', max_length=50)
+    email = models.EmailField('Email', max_length=100)
+    age = models.PositiveIntegerField('Age')
+    city = models.CharField('City', max_length=50)
+    country = models.CharField('Country', max_length=50)
+    picture = models.CharField('Picture', max_length=50)
+
+    def __str__(self) -> str:
+        return self.full_name
+    
+    class Meta():
+        verbose_name_plural = 'Users'
+        db_table = 'users'
